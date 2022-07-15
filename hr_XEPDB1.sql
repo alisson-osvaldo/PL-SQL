fbe8873b0ef9f6de39bf00fd30fe8ed35b070ad8
Alisson Osvaldo <alisson.artigas@gmail.com>
2022-07-15 00:11:47 -0300

    commit 1

--- hr_XEPDB1.sql
+++ hr_XEPDB1.sql
@@ -2,6 +2,8 @@
 SELECT sysdate
 FROM dual;
 
+-- Bloco anonimo --------------------------------------------------------------
+
 SET SERVEROUTPUT ON --abilitando o buffer de saida da packge out put
 DECLARE
     vNumero1 NUMBER(11,2) := 500;
@@ -11,3 +13,16 @@ BEGIN --Iniciar sessão executavel
     vMedia := (vNumero1 + vNumero2) / 2;
     DBMS_OUTPUT.PUT_LINE('Média: ' || vMedia); -- ||Concatena
 END;
+
+
+SET SERVEROUTPUT ON 
+DECLARE
+    vTexto VARCHAR(100):= 'Seja bem vindo ao PL/SQL';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(vTexto);
+EXCEPTION 
+    WHEN OTHERS 
+    THEN
+    DBMS_OUTPUT.PUT_LINE('Erro Oracle: ' || SQLCODE || SQLERRM);
+END;
+
