0099398f86ee47c2992fb21ff67ad7a465e3696e
Alisson Osvaldo <alisson.artigas@gmail.com>
2022-07-16 00:21:20 -0300

    definindo variáveis

--- hr_XEPDB1.sql
+++ hr_XEPDB1.sql
@@ -1,8 +1,43 @@
+-- Definindo Indentificadores - Váriaveis
+
+-- Literal Númerico --
+vvalor := 5000; --Recebe valor 5mil (decimal)
+vvalor2 := 2638E8; --Recebe (2638 * 10) ^ 8; (literal em notação Ciêntifica)
+vdouble := 2.0d; --Recebe 2.0 que é do tipo (Double)
+vfloat := 2.0f; --Recebe 2.0 que é do tipo (Float)
+
+-- Literal Boleano --
+vboleano = TRUE;
+vboleano = FALSE;
+vboleano NULL;
+
+-- Literal String --
+vstring = 'String de caractéres';
+
+/*
+-Declare e inicialize variaveis dentro da sessão "DECLARE".
+-Declarações também podem atribuir um valor inicial e atribuir a constraint "NOT NULL".
+-Para atribuir novos valores a variável deve estar dentro da sessão "BEGIN";
+
+Sintaxe para declarar váriaveis:
+
+DECLARE
+NomeIdentificador [CONSTANT] TipoDeDado     
+[NOT NULL][:= | DEFAULT expressão]; --Para inicializar a variável := ou DEFAULT eo valor;  
+*/
+
+
+
+-------------------------------------------------------------------------------
+
 --SYSDATE : função que retorna a data atual
 SELECT sysdate
 FROM dual;
 
--- Bloco anonimo --------------------------------------------------------------
+
+
+------------------------------------------------------------------------------
+--Bloco anonimo
 
 SET SERVEROUTPUT ON --abilitando o buffer de saida da packge out put
 DECLARE
@@ -26,3 +61,4 @@ EXCEPTION
     DBMS_OUTPUT.PUT_LINE('Erro Oracle: ' || SQLCODE || SQLERRM);
 END;
 
+----------------------------------------------------------------------------
