f5de9bf832f3c25491d970f62830e8a357195410
Alisson Osvaldo <alisson.artigas@gmail.com>
2022-07-26 22:34:12 -0300

    Colections

--- hr_XEPDB1.sql
+++ hr_XEPDB1.sql
@@ -692,7 +692,27 @@ BEGIN
 END;
 
 
-
+-- Associative Array of Records - Bulk Collect (não recomendado para tabelas grandes, consumo de memória)
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+DECLARE
+    TYPE employees_table_type IS TABLE OF employees%rowtype
+    INDEX BY BINARY_INTEGER; -- Type Associative Array
+    employees_table employees_table_type;
+BEGIN
+    SELECT *
+        BULK COLLECT INTO employees_table --Coleta toda a tabela para o Array;
+    FROM employees;
+    FOR i IN employees_table.first..employees_table.last  --trazer a indice do primeiro elemento da colaction até o ultimo.
+    LOOP
+       DBMS_OUTPUT.PUT_LINE(employees_table(i).employee_id || ' - ' || 
+                         employees_table(i).first_name || ' - ' || 
+                         employees_table(i).last_name || ' - ' ||
+                         employees_table(i).phone_number || ' - ' ||
+                         employees_table(i).job_id || ' - ' ||
+                         TO_CHAR(employees_table(i).salary,'99G999G999D99')); 
+    END LOOP;
+END;
 
 
 
