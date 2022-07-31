d6d0390ef9f85dd9cc5cd473fda09ca104eacfab
Alisson Osvaldo <alisson.artigas@gmail.com>
2022-07-30 23:33:53 -0300

    Tratamento de exceções

--- Query_2.sql
+++ Query_2.sql
@@ -43,26 +43,22 @@ BEGIN
     CLOSE employees_cursor; -- Close do Cursor
 END;
     
-    
+     
 -- Controlando um Cursor Explicito com WHILE LOOP
 SET SERVEROUTPUT ON
 SET VERIFY OFF
 DECLARE
   CURSOR  employees_cursor  IS
-  SELECT  *
-  FROM    employees; -- DeclaraÃ§Ã£o do Cursor
+  SELECT *
+  FROM    employees; -- Declaração do Cursor
   employees_record  employees_cursor%rowtype; 
 BEGIN
-  /* Inicializa */
-  
   OPEN  employees_cursor; -- Abrindo o Cursor
   
   FETCH  employees_cursor  
     INTO  employees_record; -- Fetch do Cursor
 	
-  /* Loop */
-  
-  WHILE  employees_cursor%found  LOOP
+  WHILE  employees_cursor%found  LOOP --Enquanto a ultima linha for TRUE 
      DBMS_OUTPUT.PUT_LINE(employees_record.employee_id || ' - ' ||
                          employees_record.first_name || ' ' || 
                          employees_record.last_name || ' - ' ||
@@ -77,6 +73,134 @@ BEGIN
   CLOSE employees_cursor; -- Close do Cursor
 END;
     
-    
-    
+  
 --Controlando um cursor explicito utilizando CURSOR FOR LOOP
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+DECLARE
+    CURSOR employees_cursor IS 
+    SELECT *
+    FROM   employees;
+BEGIN
+    FOR employees_record IN employees_cursor --Declarndo variável employees_record com a mesma estrutura do employees_cursor
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(employees_record.employee_id || ' - ' ||
+                         employees_record.first_name || ' ' || 
+                         employees_record.last_name || ' - ' ||
+                         employees_record.department_id || ' - ' ||
+                         employees_record.job_id || ' - ' ||
+                         employees_record.phone_number || ' - ' ||
+                         LTRIM(TO_CHAR(employees_record.salary, 'L99G999G999D99')));
+    END LOOP;
+END;
+
+
+-- CURSOR FOR LOOP utilizando Sub-consulta
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+BEGIN
+    FOR employees_record IN ( SELECT * FROM employees)
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(employees_record.employee_id || ' - ' ||
+                         employees_record.first_name || ' ' || 
+                         employees_record.last_name || ' - ' ||
+                         employees_record.department_id || ' - ' ||
+                         employees_record.job_id || ' - ' ||
+                         employees_record.phone_number || ' - ' ||
+                         LTRIM(TO_CHAR(employees_record.salary, 'L99G999G999D99')));
+    END LOOP;
+END;
+    
+
+--Cursor FOR LOOP com parâmetros
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+DECLARE
+    CURSOR employees_cursor(pdepartment_id NUMBER, pjob_id VARCHAR2)
+    IS
+    SELECT *
+    FROM   employees
+    WHERE  department_id = pdepartment_id AND job_id = pjob_id;
+BEGIN
+    FOR employees_record IN employees_cursor(60, 'IT_PROG') 
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(employees_record.employee_id || ' - ' ||
+                         employees_record.first_name || ' ' || 
+                         employees_record.last_name || ' - ' ||
+                         employees_record.department_id || ' - ' ||
+                         employees_record.job_id || ' - ' ||
+                         employees_record.phone_number || ' - ' ||
+                         LTRIM(TO_CHAR(employees_record.salary, 'L99G999G999D99')));
+    END LOOP;
+END;
+
+
+-- Cursor ExplÃ­cito com SELECT FOR UPDATE
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+DECLARE
+    CURSOR  employees_cursor(pjob_id VARCHAR2)
+    IS
+    SELECT *
+    FROM   employees
+    WHERE  job_id = pjob_id
+    FOR UPDATE;
+BEGIN
+    FOR employees_record IN employees_cursor('AD_VP')
+    LOOP
+        UPDATE employees
+        SET    salary = salary * (1 + 10 / 100)
+        WHERE CURRENT OF employees_cursor;
+    END LOOP;
+    COMMIT;
+END;
+
+
+------------------------------------------------------------------------------------------------------------------------------------
+--- Tratamento de Exceção ---
+--Meu código de erro vai de -20001 até -20999;
+ 
+--Excecções prédefinidas ORACLE
+/*
+Nome da exceção         Erro Oracle       Descrição
+COLLECTION_IS_NULL      ORA-06531         Tentativa de aplicar métodos que não o EXISTS para uma Collection não inicializada.
+CURSOR_ALREADY_OPEN     ORA-06511         Tentativa de abrir um cursor que já está aberto.
+DUP_VAL_ON_INDEX        ORA-00001         Tentativa de inserir um valor duplicado em um indice único.
+INVALID_CURSOR          ORA-01001         Ocorreu uma operação ilegal em um cursor.
+INVALID_NUMBER          ORA-01722         Falha na conversão de uma string caractere para númerica.
+LOGIN_DENIED            ORA-01017         Conexão ao ORACLE com um nome de usuário/senha inválida.
+NO_DATA_FOUND           ORA-01403         SELECT do tipo single-row não retornou nem uma linha.
+NOT_LOGGED_ON           ORA-01012         Programa PL/SQL executou uma chamada ao BD sem estar conectado ao ORACLE.
+TIMEOUT_ON_RESOURCE     ORA-00051         Ocorreu um time-out enquanto o ORACLE estava aguardando por um recurso.
+TOO_MANY_ROWS           ORA-01422         SELECT do tipo single-row retornou mais que uma linha.
+VALUE_ERROR             ORA-06502         Ocorreu um erro de aritmética, converão ou truncamento.
+ZERO_DIVIDE             ORA-01476         Tentativa de divisão por zero.
+*/
+
+--Tratamento de exceção pré definida ORACLE
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+ACCEPT  pEmployee_id PROMPT 'Digite o Id do Empregado: '
+DECLARE
+  vFirst_name   employees.first_name%TYPE;
+  vLast_name    employees.last_name%TYPE;
+  vEmployee_id  employees.employee_id%TYPE := &pEmployee_id;
+BEGIN
+  SELECT first_name, last_name
+  INTO   vfirst_name, vlast_name
+  FROM   employees
+  WHERE  employee_id = vEmployee_id;
+
+  DBMS_OUTPUT.PUT_LINE('Empregado: ' || vfirst_name || ' ' || vlast_name);
+ 
+EXCEPTION
+  WHEN NO_DATA_FOUND 
+  THEN
+     RAISE_APPLICATION_ERROR(-20001, 'Empregado nÃ£o encontrado, id = ' || 
+     TO_CHAR(vEmployee_id)); --Meu código de erro -20001 até 20999;
+  WHEN OTHERS 
+  THEN
+     RAISE_APPLICATION_ERROR(-20002, 'Erro Oracle - ' || SQLCODE || SQLERRM);
+
+END;
+
