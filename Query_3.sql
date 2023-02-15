04d2bae19ce16118b2e412c339e7d23bccc6d1e5
Alisson Osvaldo <alisson.artigas@gmail.com>
2023-02-14 23:12:12 -0300

    Package Body

--- Query_3.sql
+++ Query_3.sql
@@ -211,3 +211,176 @@ END PCK_EMPREGADOS;
 
 
 
+-- Criando o package Body
+/*
+-Para criar o package body vc precisa ter compilado o package specification OK.
+- ultilizar o mesmo nome do package specification.
+*/
+
+/*
+CREATE [OR REPLACE] PACKAGE BODY nome_package
+IS | AS 
+    -- declaração de variáveis (sera privada)
+    -- declaração de constantes(sera privada)
+    -- declaração de cursores  (sera privada)
+    -- declaração de exceções  (sera privada)
+    -- declaração de procedures(sera privada)
+    -- declaração de funções   (sera privada)
+    -- corpo de procedures  (colocar o corpo de todas as procedures, tanto das publicas como das privadas)    
+    -- corpo de funções (colocar o corpo de todas as funções, tanto das publicas como das privadas)
+END[nome_package];
+    
+*/
+
+-- Criando o Package Body
+create or replace PACKAGE BODY PCK_EMPREGADOS
+IS
+	PROCEDURE PRC_INSERE_EMPREGADO
+	  (pfirst_name    IN VARCHAR2,
+	   plast_name     IN VARCHAR2,
+	   pemail         IN VARCHAR2,
+	   pphone_number  IN VARCHAR2,
+	   phire_date     IN DATE DEFAULT SYSDATE,
+	   pjob_id        IN VARCHAR2,
+	   pSALARY        IN NUMBER,
+	   pCOMMICION_PCT IN NUMBER,
+	   pMANAGER_ID    IN NUMBER,
+	   pDEPARTMENT_ID IN NUMBER)
+	IS 
+	BEGIN
+	  INSERT INTO employees (
+		employee_id,
+		first_name,
+		last_name,
+		email,
+		phone_number,
+		hire_date,
+		job_id,
+		salary,
+		commission_pct,
+		manager_id,
+		department_id )
+	  VALUES (
+		employees_seq.nextval,
+		pfirst_name,
+		plast_name,
+		pemail,
+		pphone_number,
+		phire_date,
+		pjob_id,
+		psalary,
+		pcommicion_pct,
+		pmanager_id,
+		pdepartment_id );
+	EXCEPTION
+	  WHEN OTHERS THEN
+		 RAISE_APPLICATION_ERROR(-20001, 'Erro Oracle ' || SQLCODE || SQLERRM);
+	END;
+
+	PROCEDURE PRC_AUMENTA_SALARIO_EMPREGADO
+	  (pemployee_id   IN NUMBER,
+	   ppercentual    IN NUMBER)
+	IS
+	  -- Nenhuma vÃ¡riÃ¡vel declarada
+	BEGIN
+	  UPDATE employees 
+	  SET salary = salary * (1 + ppercentual / 100)
+	  WHERE employee_id = pemployee_id;
+
+	EXCEPTION
+	  WHEN OTHERS 
+	  THEN
+		 RAISE_APPLICATION_ERROR(-20001, 'Erro Oracle: ' || SQLCODE || ' - ' || SQLERRM);
+	END;
+
+	FUNCTION FNC_CONSULTA_SALARIO_EMPREGADO
+	  (pemployee_id   IN NUMBER)
+	   RETURN NUMBER
+	IS 
+	  vsalary  employees.salary%TYPE;
+	BEGIN
+	  SELECT salary
+	  INTO   vsalary
+	  FROM   employees
+	  WHERE employee_id = pemployee_id;
+	  RETURN (vsalary);
+	EXCEPTION
+	  WHEN NO_DATA_FOUND THEN 
+		  RAISE_APPLICATION_ERROR(-20001, 'Empregado inexistente');
+	  WHEN OTHERS THEN
+		 RAISE_APPLICATION_ERROR(-20002, 'Erro Oracle ' || SQLCODE || SQLERRM);
+	END;
+
+END PCK_EMPREGADOS;
+
+
+
+--Referenciando componentes de uma package
+/*
+ -Referencie um componente de uma package a partir de um componente da própria package ou
+  construções externas a ela.
+ - Somente os componentes (construções) públicos de uma package podem ser referenciados
+ por construções externas a package.
+ 
+ Sintaxe:
+    nome_package.nome_componente
+*/
+
+-- Referenciando Componentes de uma Package 
+
+BEGIN
+  PCK_EMPREGADOS.PRC_INSERE_EMPREGADO('Bob', 'Dylan','BDYLAN','515.258.4861',SYSDATE,'IT_PROG',20000,NULL,103,60);
+  COMMIT;
+END;
+
+BEGIN
+  PCK_EMPREGADOS.PRC_INSERE_EMPREGADO('John', 'Lenon','JLENON','515.244.4861',SYSDATE,'IT_PROG',15000,NULL,103,60);
+  COMMIT;
+END;
+
+
+
+
+
+
+
+---------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
